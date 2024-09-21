9468f6f6da2fe9851cfcaf20ae1e224162bd96f5
psychohight <7298@holbertonstudents.com>
2024-09-21 15:41:29 +0200

    1

---
+++ javascript/cours1.js
@@ -0,0 +1,76 @@
+//afficher quelque chose dans la console
+
+console.log("Hello World");     //Hello World
+console.log(25);                //25
+console.log(3.14);              //3.14
+console.log(true);              //true
+console.log(false);             //false
+console.log(1,2,3,4,5);         //1 2 3 4 5
+console.log("-----------------");
+//faire des calculs
+
+console.log(5 + 3);             //8
+console.log(5 - 3);             //2
+console.log(5 * 3);             //15
+console.log(5 / 3);             //1.6666666666666667
+console.log("-----------------");
+
+//utiliser des variables
+
+let a = 5;
+let b = 3;
+
+console.log(a + b);             //8
+console.log(a - b);             //2
+console.log(a * b);             //15
+console.log(a / b);             //1.6666666666666667
+console.log("-----------------");
+
+/*
+-----------------------
+*/
+
+let message = "Hello World";
+
+console.log(message.length);           // 11 (Longueur de la chaîne)
+console.log(message.toUpperCase());    // "HELLO WORLD"
+console.log(message.toLowerCase());    // "hello world"
+console.log(message.includes("World")); // true
+console.log("-----------------");
+
+//fonctions simples
+
+function addition(x, y) {
+    return x + y;
+}
+ 
+console.log(addition(10, 5));  // 15
+
+/*
+-----------------------
+*/
+
+let age = 25;
+console.log(`J'ai ${age} ans.`);    // Affiche "J'ai 25 ans."
+
+
+// Déclarer des variables
+
+let a = 10;
+let b = 5;
+
+a += b; // a vaut maintenant 15
+console.log(a); // Affiche 15
+
+
+/*
+-----------------------
+*/
+
+/*
+-----------------------
+*/
+
+/*
+-----------------------
+*/
