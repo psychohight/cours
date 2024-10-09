94467d563a23c9fe45c597440d479c73622e3935
psychohight <7298@holbertonstudents.com>
2024-10-09 18:27:49 +0200

    html

---
+++ javascript/cours/n.js
@@ -0,0 +1,18 @@
+const plantNeedsWater = function(day){
+    if(day === 'Wednesday')
+      return true;
+    else
+      return false;
+  }
+  plantNeedsWater('Tuesday');
+
+  
+  const plantNeedsWater = function(day) {
+    if (day === 'Wednesday') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  plantNeedsWater('Tuesday');
+  
